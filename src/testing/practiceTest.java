43fa64b8d3509f60169f76db623447455ff9df89
dsamuel27 <david.samuel1227@gmail.com>
2019-03-28 15:46:04 -0700

    pushing practice JUnit test to check travis integration

---
+++ src/testing/practiceTest.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class practiceTest {
+
+	public void testArithmetic() {
+		System.out.println("I hope 2 + 2 works.");
+		assertTrue( 2 + 2 == 4 );
+		}
+
+}
