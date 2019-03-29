e8f57a24054cb511397e177dfd34fb9b432323de
Bette <e_orr@u.pacific.edu>
2019-03-28 17:55:50 -0700

    Starting to work in JavaFX

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,6 +1,30 @@
 
-public class MainScreen {
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class MainScreen extends Application {
+	
+	Button startButton;
+	
 	public static void main(String[] args) {
-		System.out.println("HELLO");
+		//System.out.println("HELLO");
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("100% Complete Dorothy's App");
+		
+		startButton = new Button();
+		startButton.setText("the BUTTON");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(startButton);
+		
+		//Scene scene = new Scene(layout, );
 	}
 }
