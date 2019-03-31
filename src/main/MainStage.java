5023cd842a49907487abd17907e367520c211dde
Danilo Baledio <d_baledio@u.pacific.edu>
2019-03-31 15:30:55 -0700

    Style.css renamed because it will be a centralized style.css. Questions for the MVP have been found, written, and worded. Main Stage lightly refactored.

--- src/main/MainStage.java
+++ src/main/MainStage.java
@@ -1,48 +1,70 @@
 package main;
 
-import javafx.application.*;
-import javafx.event.*;
-import javafx.scene.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.stage.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MainStage extends Application {
 
 	public static final int SCREEN_HEIGHT = 600;
 	public static final int SCREEN_WIDTH = 600;
-	
+
+	// different scenes throughout application
+	private DifficultyScreenLayout diffScreenLayout;
+	private Scene difficultyScreen;
+
 	private Stage stage;
 	private QuizEventHandler quizAction;
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// stage initialization
 		stage = primaryStage;
-		stage.setTitle("100% incomplete Dorothy's app");
-		
+		stage.setTitle("100% incomplete Dorothy's app"); // TODO: rename the window later
+
+		// event handler initialization
+		// TODO: insert general event handler
 		quizAction = new QuizEventHandler(this);
-		
-		// declare new difficulty screen and its layout		
-		DifficultyScreenLayout diffScreenLayout = new DifficultyScreenLayout(quizAction);
-		Scene difficultyScreen = new Scene(diffScreenLayout, SCREEN_HEIGHT, SCREEN_WIDTH);
-		
-		// add style to this new sheet
-		difficultyScreen.getStylesheets().add("cssSheets/diffScreenStyle.css");
-		
-		Scene scene = difficultyScreen;
+
+		// initialize all scenes
+		sceneInitializer();
+
+		// boot into default screen
+		Scene scene = difficultyScreen; // TODO: change to menu screen
 		stage.setScene(scene);
 		stage.show();
 	}
 
-
+	// TODO: outsource to another file
 	public void nextQuestion() {
-		QuestionScreenLayout qScreenLayout = new QuestionScreenLayout("WHAT?", "No", "Yes", "Kinda", "Only on Tuesdays", quizAction);
+		QuestionScreenLayout qScreenLayout = new QuestionScreenLayout("WHAT?", "No", "Yes", "Kinda", "Only on Tuesdays",
+				quizAction);
 		Scene questionScreen = new Scene(qScreenLayout, SCREEN_HEIGHT, SCREEN_WIDTH);
-		
+
 		stage.setScene(questionScreen);
+	}
+
+	/*
+	 * Purpose: Initializes all screens in application Parameters: None Return
+	 * Values: None
+	 */
+
+	public void sceneInitializer() {
+		// declare new menu screen, its layout, and its style
+
+		// declare new difficulty screen, its layout, and its style
+		diffScreenLayout = new DifficultyScreenLayout(quizAction);
+		difficultyScreen = new Scene(diffScreenLayout, SCREEN_HEIGHT, SCREEN_WIDTH);
+		difficultyScreen.getStylesheets().add("cssSheets/style.css");
+
+		// declare new quiz screen, its layout, and its style
+
+		// declare new inspiration screen, its layout, and its style
+
+		// declare new resources screen, its layout, and its style
 	}
 }
