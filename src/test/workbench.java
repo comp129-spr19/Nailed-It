60cb5de8881fac6fc2f2ccd5d7cc17494a8f52fb
Danilo Baledio <d_baledio@u.pacific.edu>
2019-03-30 21:45:16 -0700

    Added a workbench to start playing with JSON IO. Also curious to see if the JSON JAR I added just is added to everyone else's class path.

---
+++ src/test/workbench.java
@@ -0,0 +1,13 @@
+package test;
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter; 
+import java.util.LinkedHashMap; 
+import java.util.Map; 
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject;
+
+public class workbench{
+	public static void main(String[] args) {
+		
+	}
+}
