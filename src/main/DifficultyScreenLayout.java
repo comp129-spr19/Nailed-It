58e4db9349edb2eefd84e62859be7abd8075bfe9
Danilo Baledio <d_baledio@u.pacific.edu>
2019-03-31 23:36:55 -0700

    Plugged in the MVP questions

--- src/main/DifficultyScreenLayout.java
+++ src/main/DifficultyScreenLayout.java
@@ -1,47 +1,46 @@
 package main;
 
-import javafx.application.*;
-import javafx.event.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.stage.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
 
 // Window = Stage
 // Content in Window = Scene
-public class DifficultyScreenLayout extends HBox implements EventHandler<ActionEvent>{
+public class DifficultyScreenLayout extends HBox implements EventHandler<ActionEvent> {
 
 	MainStage main;
 	ToggleButton easy, medium, hard;
 	Button next;
-	
+
 	DifficultyScreenLayout(MainStage main) {
 		super();
 		this.main = main;
-		
+
 		easy = new ToggleButton("EASY");
 		easy.setId("easy");
 		easy.setOnAction(this);
-		
+
 		medium = new ToggleButton("MEDIUM");
 		medium.setId("medium");
 		medium.setOnAction(this);
-		
+
 		hard = new ToggleButton("HARD");
 		hard.setId("hard");
 		hard.setOnAction(this);
-		
+
 		next = new Button("CONTINUE");
 		next.setId("next");
 		next.setDisable(true);
 		next.setOnAction(this);
-		
+
 		this.getChildren().addAll(easy, medium, hard, next);
-		
+
 		this.setAlignment(Pos.CENTER);
 		this.setSpacing(50);
-		
+
 	}
 
 	@Override
@@ -63,20 +62,24 @@ public class DifficultyScreenLayout extends HBox implements EventHandler<ActionE
 			}
 		}
 	}
-	
+
 	public boolean[] getDifficultySet() {
-		boolean[] difficultySet = {easy.isSelected(), medium.isSelected(), hard.isSelected()};
+		boolean[] difficultySet = { easy.isSelected(), medium.isSelected(), hard.isSelected() };
 		return difficultySet;
 	}
-	
-	
-	// returns true if any of the difficulty buttons are toggled.
+
+	/*
+	 * Function: Determines whether a difficulty button it selected 
+	 * Parameters: None
+	 * Return Value: Returns true if any of the difficulty buttons are toggled
+	 * Returns false if none of the difficulty buttons are toggled
+	 */
 	public boolean canEnableContButton() {
-		boolean[] difficultySet  = getDifficultySet();
+		boolean[] difficultySet = getDifficultySet();
 		for (int i = 0; i < difficultySet.length; i++) {
-				if (difficultySet[i]) {
-					return true;
-				}
+			if (difficultySet[i]) {
+				return true;
+			}
 		}
 		return false;
 	}
--- src/main/Questions.java
+++ src/main/Questions.java
@@ -10,9 +10,8 @@ import java.util.ArrayList;
  * 
  */
 
-
 public class Questions {
-	public static ArrayList<QuestionScreenLayout> generate(boolean[] difficulty,MainStage main){
+	public static ArrayList<QuestionScreenLayout> generate(boolean[] difficulty, MainStage main) {
 		// should be sending the easy medium hard toggled bool array
 		if (difficulty.length != 3) {
 			return null;
@@ -21,23 +20,27 @@ public class Questions {
 		boolean easy = difficulty[0];
 		boolean medium = difficulty[1];
 		boolean hard = difficulty[2];
-		
+
 		if (easy) {
-			questions.add(new QuestionScreenLayout(
-					"EASY QUESTION","A","B","C","D",main));
-					
+			questions.add(new QuestionScreenLayout("What is the time and space complexity of the quicksort algorithm?",
+					"time: O(n^2); space: O(log(n))", "time: O(n^2); space: O(nlog(n))",
+					"time: O(log(n)); space: O(n^2)", "time: O(nlog(n)); space: O(n)", main));
+
 		}
-		
+
 		if (medium) {
-			questions.add(new QuestionScreenLayout(
-					"MEDIUM QUESTION","A","B","C","D",main));
+			questions.add(new QuestionScreenLayout("What is the formula for the runtime of a recursive function",
+					"O(depth^branches)", "O(log(n))", "O(branches^depth)", "O(branches*depth)", main));
 		}
-		
+
 		if (hard) {
 			questions.add(new QuestionScreenLayout(
-					"HARD QUESTION","A","B","C","D",main));
+					"What is the time complexity of the following code?\n" + "\n" + "		boolean isPrime(int n) {\n"
+							+ "			if (n <= 0) return 0;\n" + "			else if (n == 1) return 1;\n"
+							+ "			return fib(n - 1) + fib(n - 2);\n" + "		}",
+					"O(log(n))", " O(n)", "O(2^n)", "O(n^2)", main));
 		}
-		
+
 		return questions;
 	}
 }
