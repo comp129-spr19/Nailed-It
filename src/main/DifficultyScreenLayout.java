5566c0844b370cc1a4db7f6ea80b937fb88f5a3a
Danilo Baledio <d_baledio@u.pacific.edu>
2019-04-01 09:38:47 -0700

    This didn't get dragged over with the previous commit

--- src/main/DifficultyScreenLayout.java
+++ src/main/DifficultyScreenLayout.java
@@ -7,11 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 
-// Window = Stage
-// Content in Window = Scene
 public class DifficultyScreenLayout extends HBox implements EventHandler<ActionEvent> {
 
 	MainStage main;
+
 	ToggleButton easy, medium, hard;
 	Button next;
 
@@ -43,6 +42,11 @@ public class DifficultyScreenLayout extends HBox implements EventHandler<ActionE
 
 	}
 
+	/*
+	 * Adjust application behavior off of user input
+	 * 
+	 * @param e User input
+	 */
 	@Override
 	public void handle(ActionEvent e) {
 		if (e.getSource() instanceof Button) {
@@ -50,12 +54,13 @@ public class DifficultyScreenLayout extends HBox implements EventHandler<ActionE
 			switch (clicked.getId()) {
 			case "next":
 				main.genQuestions(getDifficultySet());
+				// create QuizHandler Object
 				break;
 			default:
-				System.out.println("Hey what did you just do it's not in the EventHandler");
+				System.out.println("ERROR: No input case in EventHandler");
 			}
 		} else if (e.getSource() instanceof ToggleButton) {
-			if (canEnableContButton()) {
+			if (isDifficultySelected()) {
 				next.setDisable(false);
 			} else {
 				next.setDisable(true);
@@ -63,18 +68,39 @@ public class DifficultyScreenLayout extends HBox implements EventHandler<ActionE
 		}
 	}
 
-	public boolean[] getDifficultySet() {
-		boolean[] difficultySet = { easy.isSelected(), medium.isSelected(), hard.isSelected() };
-		return difficultySet;
+	// TODO: Determine if this method is usable
+	/*
+	 * Initializes difficulty button settings
+	 * 
+	 * @param button The button object to be initialized
+	 * 
+	 * @param buttonLabel The front facing button label seen by the user
+	 * 
+	 * @param buttonID The ID used by the application to identify the button
+	 */
+	public void createDifficultyOptionButton(ToggleButton button, String buttonLabel, String buttonID) {
+		button = new ToggleButton(buttonLabel);
+		button.setId(buttonID);
+		button.setOnAction(this);
 	}
 
 	/*
-	 * Function: Determines whether a difficulty button it selected 
-	 * Parameters: None
-	 * Return Value: Returns true if any of the difficulty buttons are toggled
-	 * Returns false if none of the difficulty buttons are toggled
+	 * Returns an array of each difficulty button's selection status
+	 * 
+	 * @return An array of booleans flagging each difficulty button's selection
+	 * status
 	 */
-	public boolean canEnableContButton() {
+	public boolean[] getDifficultySet() {
+		return new boolean[] { easy.isSelected(), medium.isSelected(), hard.isSelected() };
+	}
+
+	/*
+	 * Determines whether a difficulty button is selected
+	 * 
+	 * @return true if any of the difficulty buttons are toggled, false if none of
+	 * the difficulty buttons are toggled
+	 */
+	public boolean isDifficultySelected() {
 		boolean[] difficultySet = getDifficultySet();
 		for (int i = 0; i < difficultySet.length; i++) {
 			if (difficultySet[i]) {
