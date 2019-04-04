dfd193e27ab07715f70226a8aefda7b4eef4e898
Bette <e_orr@u.pacific.edu>
2019-04-03 19:50:31 -0700

    QuizScreen now gives user instant feedback based on answer button pushed

--- src/main/java/QuizScreenLayout.java
+++ src/main/java/QuizScreenLayout.java
@@ -1,21 +1,18 @@
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.*;
+import javafx.event.*;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
 
 public class QuizScreenLayout extends VBox implements EventHandler<ActionEvent> {
 
 	MainStage main;
 	
-	private RadioButton answerA, answerB, answerC, answerD;
-	private Answer correctAnswer;
-	private ToggleGroup answers;
+	private Text questionText, responseText;
+	private Button answerA, answerB, answerC, answerD;
+	private Button next, skip, quit;
+	private Answer correctAnswer, selectedAnswer;
 	
 	public QuizScreenLayout(String question, String ansrA, String ansrB, String ansrC, String ansrD, Answer answer, MainStage main) {
 		super();
@@ -23,44 +20,40 @@ public class QuizScreenLayout extends VBox implements EventHandler<ActionEvent> 
 		
 		correctAnswer = answer;
 		
-		Text questionText = new Text(question);
+		questionText = new Text(question);
 
-		answers = new ToggleGroup();
-
-		answerA = new RadioButton("A: " + ansrA);
+		answerA = new Button("A: " + ansrA);
 		answerA.setId("A");
-		answerA.setToggleGroup(answers);
 		answerA.setOnAction(this);
 
-		 answerB = new RadioButton("B: " + ansrB);
+		answerB = new Button("B: " + ansrB);
 		answerB.setId("B");
-		answerB.setToggleGroup(answers);
 		answerB.setOnAction(this);
 
-		answerC = new RadioButton("C: " + ansrC);
+		answerC = new Button("C: " + ansrC);
 		answerC.setId("C");
-		answerC.setToggleGroup(answers);
 		answerC.setOnAction(this);
 
-		 answerD = new RadioButton("D: " + ansrD);
+		answerD = new Button("D: " + ansrD);
 		answerD.setId("D");
-		answerD.setToggleGroup(answers);
 		answerD.setOnAction(this);
+		
+		responseText = new Text("");
 
-		Button next = new Button("Next");
+		next = new Button("Next");
 		next.setId("next");
 		next.setOnAction(this);
-		
+		next.setDisable(true);
 
-		Button skip = new Button("Skip");
+		skip = new Button("Skip");
 		skip.setId("skip");
 		skip.setOnAction(this);
 
-		Button quit = new Button("Return to Menu");
+		quit = new Button("Return to Menu");
 		quit.setId("quit");
 		quit.setOnAction(this);
 
-		this.getChildren().addAll(questionText, answerA, answerB, answerC, answerD, next, skip, quit);
+		this.getChildren().addAll(questionText, answerA, answerB, answerC, answerD, responseText, next, skip, quit);
 
 		this.setAlignment(Pos.CENTER);
 		this.setSpacing(10);
@@ -68,6 +61,7 @@ public class QuizScreenLayout extends VBox implements EventHandler<ActionEvent> 
 	}
 
 	/*
+	 * 
 	 * Adjust application behavior off of user input
 	 * 
 	 * @param e User input
@@ -77,11 +71,25 @@ public class QuizScreenLayout extends VBox implements EventHandler<ActionEvent> 
 		if (e.getSource() instanceof Button) {
 			Button clicked = (Button) e.getSource();
 			switch (clicked.getId()) {
+			case "A":
+				selectedAnswer = Answer.ANSWER_A;
+				handleSelection();
+				break;
+			case "B":
+				selectedAnswer = Answer.ANSWER_B;
+				handleSelection();
+				break;
+			case "C":
+				selectedAnswer = Answer.ANSWER_C;
+				handleSelection();
+				break;
+			case "D":	
+				selectedAnswer = Answer.ANSWER_D;
+				handleSelection();
+				break;
 			case "next":
-
-				checkAnswers();
+				mainStageCommunication();
 				main.nextQuestion();
-
 				break;
 			case "skip":
 				main.nextQuestion();
@@ -95,38 +103,44 @@ public class QuizScreenLayout extends VBox implements EventHandler<ActionEvent> 
 		}
 	}
 	
-	private void checkAnswers() {
-		ObservableList<Toggle> answerToggle = answers.getToggles();
-		for (int i = 0; i < answers.getToggles().size(); i++) {
-			if (answerToggle.get(i).isSelected()) {
-				if (validated(i)) {
-				main.incrCorrAnswers();
-				break;
-				}
-			}
-		}
+	/*
+	 * Displays response text based on answer,
+	 * then disables/enables appropriate buttons
+	 * 
+	 * */
+	private void handleSelection() {
+		setResponse();
+		next.setDisable(false);
+		disableAnswers();
 	}
 	
-	private boolean validated(int index) {
-		Answer selectedAnswer;
-		switch(index) {
-		case 0:
-			selectedAnswer = Answer.ANSWER_A;
-			break;
-		case 1:
-			selectedAnswer = Answer.ANSWER_B;
-			break;
-		case 2:
-			selectedAnswer = Answer.ANSWER_C;
-			break;
-		case 3:	
-			selectedAnswer = Answer.ANSWER_D;
-			break;
-		default:
-			selectedAnswer = null;
+	/*
+	 * Disables the full set of answer buttons
+	 * */	
+	private void disableAnswers() {
+		answerA.setDisable(true);
+		answerB.setDisable(true);
+		answerC.setDisable(true);
+		answerD.setDisable(true);
+	}
+	
+	/*
+	 * Validates answer, then displays to user using responseText
+	 * */
+	private void setResponse() {
+		String response = new String("Incorrect.");
+		if (selectedAnswer == correctAnswer) {
+			response = "Correct.";
 		}
-		
-		return (selectedAnswer == correctAnswer);
+		responseText.setText(response);
 	}
 
+	/*
+	 * Increments correct answer in MainStage if needed
+	 * */
+	private void mainStageCommunication() {
+		if (selectedAnswer == correctAnswer) {
+			main.incrCorrAnswers();
+		}
+	}
 }
