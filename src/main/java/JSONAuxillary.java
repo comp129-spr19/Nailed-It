7848c3d104bb1261d40d6a045558f7632132441f
dsamuel27 <david.samuel1227@gmail.com>
2019-04-05 16:54:21 -0700

    Changed name of JSONParser to JSONAuxillary

--- src/main/java/JSONAuxillary.java
+++ src/main/java/JSONAuxillary.java
@@ -3,10 +3,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.json.simple.parser.ParseException;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
-public class JSONParser {
+public class JSONAuxillary {
 
+	
+	public JSONObject getJSONObject() {
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		JSONParser parser = new JSONParser();
 		
