cedd2910d81806b5ae5897b09969830b315257b3
Steven <s_melavic@u.pacific.edu>
2019-04-04 21:11:11 -0700

    Initializing JSON writer class for Java.

---
+++ src/main/java/JSONWriter.java
@@ -0,0 +1,13 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JSONWriter {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
