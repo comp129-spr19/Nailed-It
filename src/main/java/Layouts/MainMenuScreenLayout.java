02898d7aabd4f5458b9aae2d9d7dba6d86631da8
Ivan <ivan.lopezamaral@gmail.com>
2019-04-08 17:32:38 -0700

    Working on MainMenuScreen prototype

--- src/main/java/Layouts/MainMenuScreenLayout.java
+++ src/main/java/Layouts/MainMenuScreenLayout.java
@@ -1,1 +1,34 @@
 package Layouts;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import main.MainStage;
+
+public class MainMenuScreenLayout extends VBox  implements EventHandler<ActionEvent>{
+
+	private Button quiz;
+	private Button editor;
+	private MainStage main;
+	
+	@Override
+	public void handle(ActionEvent event) {
+
+	}
+	
+	public MainMenuScreenLayout(MainStage main) {
+		super();
+		this.main = main;
+		
+		quiz = new Button("Quiz");
+		quiz.setId("quiz");
+		editor = new Button("Editor");
+		editor.setId("editor");
+
+		this.getChildren().addAll(quiz, editor);
+	}
+	
+	
+	
+}
