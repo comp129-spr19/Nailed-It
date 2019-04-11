e62f34d90babcc11ac7ffdf7cb81f0ae282f425c
Danilo Baledio <d_baledio@u.pacific.edu>
2019-04-10 19:00:30 -0700

    Default case in AnswerConverter now returns null instead of Answer_A

--- src/main/java/main/AnswerConverter.java
+++ src/main/java/main/AnswerConverter.java
@@ -1,34 +1,33 @@
 package main;
 
 public class AnswerConverter {
-public static Answer stringToAnswer(String answer) {
-		
+	public static Answer stringToAnswer(String answer) {
+
 		answer = answer.toUpperCase();
 		Answer selectedAnswer;
 		switch (answer) {
-		case "A" :
+		case "A":
 			selectedAnswer = Answer.ANSWER_A;
-		
+
 			break;
 		case "B":
 			selectedAnswer = Answer.ANSWER_B;
-		
+
 			break;
 		case "C":
 			selectedAnswer = Answer.ANSWER_C;
-			
+
 			break;
-		case "D":	
+		case "D":
 			selectedAnswer = Answer.ANSWER_D;
-		
+
 			break;
-	
+
 		default:
-			selectedAnswer = Answer.ANSWER_A;
-			
-			
-	}
+			selectedAnswer = null;
+
+		}
 		return selectedAnswer;
 	}
-	
+
 }
