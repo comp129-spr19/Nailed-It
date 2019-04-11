5a2b829e4a9ac9caead74882d005429391d22399
dsamuel27 <david.samuel1227@gmail.com>
2019-04-11 13:44:43 -0400

    Can pull xml from web page

--- src/main/java/Scrapper/Scrapper.java
+++ src/main/java/Scrapper/Scrapper.java
@@ -1,5 +1,12 @@
 package Scrapper;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
+import com.gargoylesoftware.*;
 
 public class Scrapper {
 	public static String baseUrl = "https://www.geeksforgeeks.org/algorithms-gq/analysis-of-algorithms-gq/";
@@ -9,6 +16,21 @@ public static void main(String args[]) {
 
 	client.getOptions().setCssEnabled(false);
 	client.getOptions().setJavaScriptEnabled(false);
+	
+	
+		
+		HtmlPage page;
+		try {
+			page = client.getPage(baseUrl);
+			System.out.println(page.asXml());
+		} catch (FailingHttpStatusCodeException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+	
 	
 }
 
