6cd0f473d0bf3a5f79bb5ab95bcb784f68f9686c
dsamuel27 <david.samuel1227@gmail.com>
2019-04-12 13:34:29 -0400

    JUnit test for Scrapper added. Added powermock dependency

---
+++ src/test/java/ScrapperTester.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.Answer;
+import main.Question;
+
+import org.powermock.*;
+import org.powermock.api.mockito.PowerMockito;
+
+import Scrapper.Scrapper;
+public class ScrapperTester {
+	
+	/*
+	@Before
+	public void setUp() {
+		PowerMockito.mockStatic(Scrapper.class);
+		
+		ArrayList<Question> sample = new ArrayList<Question>();
+		
+		for (int i = 0; i < 10; i++) {
+			sample.add(new Question("test","1","2","3","4","hint",Answer.ANSWER_A));
+		}
+		PowerMockito.when(Scrapper.getQuestions("https://www.geeksforgeeks.org/algorithms-gq/graph-shortest-paths-gq/")).thenReturn(sample);
+		PowerMockito.when(Scrapper.getQuestions("https://www.geeksforgeeks.org/algorithms-gq/searching-and-sorting-gq/")).thenReturn(sample);
+	}
+	*/
+	@Test public void testSiteOne() {
+		for (Question q :Scrapper.getQuestions("https://www.geeksforgeeks.org/algorithms-gq/graph-shortest-paths-gq/")) {
+			assertNotNull(q);
+		}
+		
+		
+	} 
+	
+	@Test public void testSiteTwo() {
+		for (Question q :Scrapper.getQuestions("https://www.geeksforgeeks.org/algorithms-gq/searching-and-sorting-gq/")) {
+			assertNotNull(q);
+		}
+	}
+	
+	
+	//public ArrayList<Question> scrapperGettingQuestions() {
+		
+	//}
+	
+
+}
