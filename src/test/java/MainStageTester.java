d79e8ed811a781b00b012a3ca951c184117d4e57
Steven <s_melavic@u.pacific.edu>
2019-04-11 23:01:37 -0700

    Starting JUnit tests for MainStage.

---
+++ src/test/java/MainStageTester.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import Layouts.EditorScreenLayout;
+import javafx.scene.Scene;
+
+import static org.junit.Assert.;
+import static org.mockito.Mockito.when;
+
+import main.MainStage;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainStageTester {
+	@Test
+	public void testSceneRootIsSetToEditorScreenLayoutWhenStartEditorIsCalled() {
+		assertTrue(1 == 1);
+	}
+}
