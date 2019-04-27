7b7493c89ef92c799d3bb8ee75519caeb61cb739
Bette <e_orr@u.pacific.edu>
2019-04-26 21:23:44 -0700

    Quiz Screen looks a tiny bit different now

--- src/main/java/Layouts/QuizScreenLayout.java
+++ src/main/java/Layouts/QuizScreenLayout.java
@@ -1,7 +1,5 @@
 package Layouts;
 
-import java.io.File;
-
 import javafx.event.*;
 import javafx.geometry.*;
 import javafx.scene.control.*;
@@ -12,119 +10,187 @@ import main.Answer;
 import main.MainStage;
 import main.Question;
 
-public class QuizScreenLayout extends BorderPane implements EventHandler<ActionEvent> {
+public class QuizScreenLayout extends GridPane implements EventHandler<ActionEvent> {
 	public static final int MAX_ATTEMPTS = 2;
-	public static final String IMAGE_FILEPATH = "file:src/main/resources/images/";
-
+	public static final String FILEPATH = "file:src/main/resources/";
+	public static final String IMAGE_FILEPATH = FILEPATH + "images/";
+	public static final String EXPLANATIONS_FILEPATH = FILEPATH + "explanations/";
+	
 	MainStage main;
-
-	private Text questionText, responseText, hintText,divisor,questionNumber,totalQuestions, attemptedString;
-	private Button answerA, answerB, answerC, answerD;
-	private Button next, skip, quit, hint, explanation;
-	private Answer correctAnswer, selectedAnswer;
-	private String hintString, explanationString;
-	private int numAttempts;
-	private boolean complete;
-	private HBox header;
-	private ImageView picture;
-
+	Question question;
+	
+	int numAttempts;
+	boolean complete;
+	Answer selectedAnswer;
+	
+	HBox menu;
+	Button quit, hint, skip, next, explanation;
+	HBox header;
+	Text divisor, questionNumber, totalQuestions, attemptedString;
+	
+	HBox answers;
+	Button answerA, answerB, answerC, answerD;
+	
+	ScrollPane questionScroller;
+	VBox questionVBox;
+	Image img;
+	ImageView image;
+	Text questionText, hintText;
+	
+	ScrollPane answerScroller;
+	VBox answerVBox;
+	Text responseText, answersText;
+	
 	public QuizScreenLayout(Question question, MainStage main) {
 		super();
 		this.main = main;
+		this.question = question;
 		numAttempts = 0;
 		complete = false;
-
-		header = new HBox();
-		divisor = new Text("/");
-		questionNumber = new Text("");
-		totalQuestions = new Text("");
-		attemptedString = new Text("");
-		header.getChildren().addAll(questionNumber,divisor,totalQuestions);
-		this.setTop(header);
 		
+		this.setPadding(new Insets(0, 0, 0, 0));
+		this.setAlignment(Pos.CENTER);
+		this.setVgap(0);
+		this.setHgap(0);
 		
+		createMenu();
+		setUpHBox(menu, main.getScreenWidth() / 2, 100);
+		GridPane.setConstraints(menu, 0, 0);
 		
+		createAnswers();
+		setUpHBox(answers, main.getScreenWidth() / 2, 100);
+		GridPane.setConstraints(answers, 1, 0);
 		
-		GridPane answerSelection = setMultipleChoiceOptions(question);
-		this.setBottom(answerSelection);
-
-		/*************/
-		HBox hbox = new HBox();
-		hbox.setPadding(new Insets(10));
-		hbox.setSpacing(8);
-
+		createQuestionScroller();
+		setUpScrollPane(questionScroller, main.getScreenWidth()/2, main.getScreenHeight() - 110);
+		GridPane.setConstraints(questionScroller, 0, 1);
+		
+		createAnswerScroller();
+		setUpScrollPane(answerScroller, main.getScreenWidth()/2, main.getScreenHeight() - 110);
+		GridPane.setConstraints(answerScroller, 1, 1);
+		
+		this.getChildren().addAll(menu, answers, questionScroller, answerScroller);
+	}
+	
+	private void setUpHBox(HBox box, double width, double height) {
+		box.setMaxHeight(height);
+		box.setMinHeight(height);
+		box.setMaxWidth(width);
+		box.setMinWidth(width);
+	}
+	
+	private void setUpScrollPane(ScrollPane pane, double width, double height) {
+		pane.setMaxHeight(height);
+		pane.setMinHeight(height);
+		pane.setMaxWidth(width);
+		pane.setMinWidth(width);
+	}
+	
+	private void createMenu() {
+		menu = new HBox();
+		menu.setPadding(new Insets(10));
+		menu.setSpacing(8);
+		menu.setAlignment(Pos.BOTTOM_CENTER);
+		
 		quit = new Button("Quit");
 		quit.setId("quit");
 		quit.setOnAction(this);
+		menu.getChildren().add(quit);
 
+		if (question.getHint() != "None") {
+			hint = new Button("Hint");
+			hint.setId("hint");
+			hint.setOnAction(this);
+			menu.getChildren().add(hint);
+		}
+		
 		skip = new Button("Skip");
 		skip.setId("skip");
 		skip.setOnAction(this);
+		menu.getChildren().add(skip);
 
 		next = new Button("Next");
 		next.setId("next");
 		next.setOnAction(this);
 		next.setDisable(true);
-
-		hbox.getChildren().add(quit);
-		if ("NO HINTS WITH GEEKSFORGEEKS".equals(question.getHint())) {
-			hint = null;
-			// because hint is null, do not add to hbox
-		} else {
-			hint = new Button("Hint");
-			hint.setId("hint");
-			hint.setOnAction(this);
-			hintString = question.getHint();
-			hbox.getChildren().add(hint);
-		}
-		hbox.getChildren().addAll(skip, next);
+		menu.getChildren().add(next);
 		
 		if (question.getExplanation() != "") {
 			explanation = new Button("Explanation");
 			explanation.setId("explain");
 			explanation.setOnAction(this);
 			explanation.setDisable(true);
-			explanationString = question.getExplanation();
-			hbox.getChildren().add(explanation);
+			menu.getChildren().add(explanation);
 		}
-		hbox.getChildren().addAll(header, attemptedString);
-
-		hbox.setAlignment(Pos.BOTTOM_CENTER);
-
-		/*************/
-
-		VBox vbox = new VBox();
-		vbox.setPadding(new Insets(10));
-		vbox.setSpacing(8);
 		
-		if (question.getImage() != "") {
-			Image image = new Image(IMAGE_FILEPATH + question.getImage());
-			picture = new ImageView(image);
-			picture.setVisible(true);
-			vbox.getChildren().add(picture);
-		}
-
-		Label questionText = new Label(question.getQuestion());
-		questionText.setWrapText(true);
-		questionText.setContentDisplay(ContentDisplay.CENTER);
-
-		hintText = new Text("");
-		correctAnswer = question.getCorrectAnswer();
-		responseText = new Text("");
-
-		vbox.setAlignment(Pos.BOTTOM_CENTER);
-
-		vbox.getChildren().addAll(hbox, new ScrollPane(questionText), hintText, responseText);
-
-		this.setCenter(vbox);
+		header = new HBox();
+		divisor = new Text("/");
+		questionNumber = new Text("");
+		totalQuestions = new Text("");
+		header.getChildren().addAll(questionNumber,divisor,totalQuestions);
+		attemptedString = new Text("");
+		menu.getChildren().addAll(header, attemptedString);
 	}
 
-	/*
-	 * 
-	 * Adjust application behavior off of user input
-	 * 
-	 * @param e User input
-	 */
+	private void createAnswers() {
+		answers = new HBox();
+		
+		answerA = new Button("A");
+		answerA.setId("A");
+		answerA.setOnAction(this);
+		
+		answerB = new Button("B");
+		answerB.setId("B");
+		answerB.setOnAction(this);
+		
+		answerC = new Button("C");
+		answerC.setId("C");
+		answerC.setOnAction(this);
+		
+		answerD = new Button("D");
+		answerD.setId("D");
+		answerD.setOnAction(this);
+		
+		answers.getChildren().addAll(answerA, answerB, answerC, answerD);
+	}
+
+	private void createQuestionScroller() {
+		questionScroller = new ScrollPane();
+		questionVBox = new VBox();
+
+		hintText = new Text("");
+		questionVBox.getChildren().add(hintText);
+		
+		if (question.getImage() != "") {
+			img = new Image(IMAGE_FILEPATH + question.getImage());
+			image = new ImageView(img);
+			questionVBox.getChildren().add(image);
+		}
+
+		questionText = new Text(question.getQuestion());
+		questionVBox.getChildren().add(questionText);
+		
+		questionScroller.setContent(questionVBox);
+	}
+
+	private void createAnswerScroller() {
+		answerScroller = new ScrollPane();
+		answerVBox = new VBox();
+		
+		responseText = new Text("");
+		
+		String answersString = "A: " + question.getAnswerA() + "\n\n";
+		answersString += "B: " + question.getAnswerB() + "\n\n";
+		answersString += "C: " + question.getAnswerC() + "\n\n";
+		answersString += "D: " + question.getAnswerD() + "\n\n";
+		
+		answersText = new Text(answersString);
+		
+		answerVBox.getChildren().addAll(responseText, answersText);
+		answerScroller.setContent(answerVBox);
+	}
+
+
 	@Override
 	public void handle(ActionEvent e) {
 		if (e.getSource() instanceof Button) {
@@ -164,71 +230,24 @@ public class QuizScreenLayout extends BorderPane implements EventHandler<ActionE
 				showHint();
 				break;
 			case "explain":
-				main.switchToExplanation(explanationString);
+				main.switchToExplanation(question.getExplanation());
+				break;
 			default:
 				System.out.println("ERROR: No input case in EventHandler");
 			}
 		}
 	}
 
-	/*
-	 * 
-	 */
-	private GridPane setMultipleChoiceOptions(Question question) {
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(0, 0, 0, 0));
-		grid.setAlignment(Pos.CENTER);
-		grid.setVgap(0);
-		grid.setHgap(0);
-		double height = 75;
-		double width = main.getScreenWidth() / 2;
-
-		answerA = new Button("A: " + question.getAnswerA());
-		answerA.setId("A");
-		setUpButton(answerA, height, width);
-		GridPane.setConstraints(answerA, 0, 0);
-
-		answerB = new Button("B: " + question.getAnswerB());
-		answerB.setId("B");
-		setUpButton(answerB, height, width);
-		GridPane.setConstraints(answerB, 1, 0);
-
-		answerC = new Button("C: " + question.getAnswerC());
-		answerC.setId("C");
-		setUpButton(answerC, height, width);
-		GridPane.setConstraints(answerC, 0, 1);
-
-		answerD = new Button("D: " + question.getAnswerD());
-		answerD.setId("D");
-		setUpButton(answerD, height, width);
-		GridPane.setConstraints(answerD, 1, 1);
-
-		grid.getChildren().addAll(answerA, answerB, answerC, answerD);
-
-		return grid;
-	}
-	
-	private void setUpButton(Button button, double height, double width) {
-		button.setMaxHeight(height);
-		button.setMinHeight(height);
-		button.setMaxWidth(width);
-		button.setMinWidth(width);
-		button.setOnAction(this);
-	}
-
-	/*
-	 * Displays response text based on answer, then disables/enables appropriate
-	 * buttons
-	 * 
-	 */
 	private void handleSelection() {
 		numAttempts++;
 		attemptedString.setText("Attempted: " + numAttempts + "/" + MAX_ATTEMPTS);
-		complete = (selectedAnswer == correctAnswer) || (numAttempts >= MAX_ATTEMPTS);
+		complete = (selectedAnswer.equals(question.getCorrectAnswer())) || (numAttempts >= MAX_ATTEMPTS);
 		setResponse();
 		if (complete) {
 			next.setDisable(false);
-			explanation.setDisable(false);
+			if (explanation != null) {
+				explanation.setDisable(false);
+			}
 			skip.setDisable(true);
 			disableAnswers();
 		}
@@ -250,9 +269,9 @@ public class QuizScreenLayout extends BorderPane implements EventHandler<ActionE
 	private void setResponse() {
 		String response = new String("Incorrect.");
 		if (complete) {
-			response += " Answer was: " + correctAnswer.toString();
+			response += " Answer was: " + question.getCorrectAnswer().toString();
 		}
-		if (selectedAnswer == correctAnswer) {
+		if (selectedAnswer == question.getCorrectAnswer()) {
 			response = "Correct.";
 		}
 		responseText.setText(response);
@@ -262,7 +281,7 @@ public class QuizScreenLayout extends BorderPane implements EventHandler<ActionE
 	 * Increments correct answer in MainStage if needed
 	 */
 	private void mainStageCommunication() {
-		if (selectedAnswer == correctAnswer) {
+		if (selectedAnswer == question.getCorrectAnswer()) {
 			main.incrCorrAnswers();
 		}
 	}
@@ -271,7 +290,7 @@ public class QuizScreenLayout extends BorderPane implements EventHandler<ActionE
 	 * Sets the hintText variable
 	 */
 	private void showHint() {
-		hintText.setText(hintString);
+		hintText.setText("Hint: " + question.getHint());
 	}
 	
 	public void setQuestionCounterText(int qNumber, int totalQuestions) {
