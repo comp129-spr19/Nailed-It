298bd7d5379e4e5d39435f8fb0afd9717f6ebc87
dsamuel27 <david.samuel1227@gmail.com>
2019-04-30 00:08:33 -0700

    Color util test done

--- src/test/java/ColorUtilTester.java
+++ src/test/java/ColorUtilTester.java
@@ -15,5 +15,22 @@ public class ColorUtilTester {
 	@Test
 	public void returnDefaultValue() {
 		assertEquals(ColorUtil.editorColor(""),"-fx-color: rgb(238,247,121)" );
+		assertEquals(ColorUtil.editorColor("  "),"-fx-color: rgb(238,247,121)" );
+		assertEquals(ColorUtil.editorColor("testing"),"-fx-color: rgb(238,247,121)" );
+	}
+	
+	@Test
+	public void shouldReturnAlgorithmsColor() {
+		assertEquals(ColorUtil.editorColor("Algorithms"),"-fx-color: rgb(121,169,247)" );
+	}
+	
+	@Test
+	public void shouldReturnDataStructuresColor() {
+		assertEquals(ColorUtil.editorColor("Data Structures"),"-fx-color: rgb(121,230,247)" );
+	}
+	
+	@Test
+	public void shouldReturnCustomColor() {
+		assertEquals(ColorUtil.editorColor("Custom"),"-fx-color: rgb(121,247,159)" );
 	}
 }
